76bee0ee63b9faa007acb16b204c7a4392b32f9e
Pusztai <kondi.elte@gmail.com>
2019-09-24 14:49:23 +0200

    Upgrade node to 12.10.0

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:12.4.0
+FROM node:12.10.0
 
 # Xvfb
 
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 docker build -t node-chrome .
-docker tag node-chrome kondi1/node-chrome:10.15.0
+docker tag node-chrome kondi1/node-chrome:12.10.0
